99db84cae1a14b4e988b30ef76b0953f86d16756
junghyun <adanam872@gmail.com>
2018-08-30 12:43:01 +0900

    make exit link(to list.html) in myclass/lecture.html

--- src/main/webapp/html/Lecture/list.js
+++ src/main/webapp/html/Lecture/list.js
@@ -12,7 +12,9 @@ $.getJSON(`${serverApiAddr}/json/lecture/totalpage`, {}).done(function (result) 
 
 function loadList() {
 	console.log('로드됨!');
-	$.getJSON(`${serverApiAddr}/json/lecture/list`, {'page' : page}).done(function (result) {
+	$.getJSON(`${serverApiAddr}/json/lecture/list`, {
+		'page': page
+	}).done(function (result) {
 		console.log(result.status);
 		console.log(result.list);
 		data = result;
@@ -32,29 +34,43 @@ function loadList() {
 			`);
 		};
 		console.log(data.list);
-		for (var item of data.list) {
-			console.log(item);
-			$("<tr>").html(` <th scope="row">${item.lectureNo}</th>
-                        <td>${item.lectureSubject}</td>
-                        <td><a href="lectureinfo.html" data-no="${item.lectureNo}" data-no2="${item.pNum}" class="viewLink">${item.lectureName}</a></td>
-                        <td>${item.lectureRoom}</td>
-                        <td>${item.lectureStartDay}</td>
-                        <td>${item.lectureEndDay}</td>
-                        <td>${item.lectureMember}/${item.lectureMaxMember}</td>
-                        <td><button type="button">신청</button>`).appendTo(tbody);
+		if (userDiv == 'professor') {
+			for (var item of data.list) {
+				console.log(item);
+				$("<tr>").html(` <th scope="row">${item.lectureNo}</th>
+							<td>${item.lectureSubject}</td>
+							<td><a href="lectureinfo.html" data-no="${item.lectureNo}" data-no2="${item.pNum}" class="viewLink">${item.lectureName}</a></td>
+							<td>${item.lectureRoom}</td>
+							<td>${item.lectureStartDay}</td>
+							<td>${item.lectureEndDay}</td>
+							<td>${item.lectureMember}/${item.lectureMaxMember}</td>
+							<td></td>`).appendTo(tbody);
+			}
+		} else {
+			for (var item of data.list) {
+				console.log(item);
+				$("<tr>").html(` <th scope="row">${item.lectureNo}</th>
+							<td>${item.lectureSubject}</td>
+							<td><a href="lectureinfo.html" data-no="${item.lectureNo}" data-no2="${item.pNum}" class="viewLink">${item.lectureName}</a></td>
+							<td>${item.lectureRoom}</td>
+							<td>${item.lectureStartDay}</td>
+							<td>${item.lectureEndDay}</td>
+							<td>${item.lectureMember}/${item.lectureMaxMember}</td>
+							<td><button type="button">신청</button></td>`).appendTo(tbody);
+			}
 		}
 	});
 }
 
 loadList();
 
-$('#nextPage').click(function() {
+$('#nextPage').click(function () {
 	if (page >= totalpage) return;
 	page++;
 	loadList();
 });
 
-$('#prevPage').click(function() {
+$('#prevPage').click(function () {
 	if (page <= 1) return;
 	page--;
 	loadList();
@@ -68,25 +84,40 @@ tbody.on('click', 'a.viewLink', function (event) {
 });
 
 $("#searchBtn").click(function () {
+	page = 1;
 	tbody.html('');
 	$.getJSON(`${serverApiAddr}/json/lecture/listsearch`, {
-		'keyword' : $('#search-input').val()
+		'keyword': $('#search-input').val()
 	}).done(function (result) {
 		console.log(result.status);
 		console.log(result.list);
 		data = result;
 		console.log(data.list);
-
-		for (var item of data.list) {
-			console.log(item);
-			$("<tr>").html(` <th scope="row">${item.lectureNo}</th>
-                        <td>${item.lectureSubject}</td>
-                        <td><a href="lectureinfo.html" data-no="${item.lectureNo}" data-no2="${item.pNum}" class="viewLink">${item.lectureName}</a></td>
-                        <td>${item.lectureRoom}</td>
-                        <td>${item.lectureStartDay}</td>
-                        <td>${item.lectureEndDay}</td>
-                        <td>${item.lectureMember}/${item.lectureMaxMember}</td>
-                        <td><button type="button">신청</button>`).appendTo(tbody);
+		totalpage = 1;
+		if (userDiv == 'professor') {
+			for (var item of data.list) {
+				console.log(item);
+				$("<tr>").html(` <th scope="row">${item.lectureNo}</th>
+							<td>${item.lectureSubject}</td>
+							<td><a href="lectureinfo.html" data-no="${item.lectureNo}" data-no2="${item.pNum}" class="viewLink">${item.lectureName}</a></td>
+							<td>${item.lectureRoom}</td>
+							<td>${item.lectureStartDay}</td>
+							<td>${item.lectureEndDay}</td>
+							<td>${item.lectureMember}/${item.lectureMaxMember}</td>
+							<td></td>`).appendTo(tbody);
+			}
+		} else {
+			for (var item of data.list) {
+				console.log(item);
+				$("<tr>").html(` <th scope="row">${item.lectureNo}</th>
+							<td>${item.lectureSubject}</td>
+							<td><a href="lectureinfo.html" data-no="${item.lectureNo}" data-no2="${item.pNum}" class="viewLink">${item.lectureName}</a></td>
+							<td>${item.lectureRoom}</td>
+							<td>${item.lectureStartDay}</td>
+							<td>${item.lectureEndDay}</td>
+							<td>${item.lectureMember}/${item.lectureMaxMember}</td>
+							<td><button type="button">신청</button></td>`).appendTo(tbody);
+			}
 		}
 	});
 });
